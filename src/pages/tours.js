e66617b54e0032735ba1d410ed959874b61c7122
Methamorphe <methamorphe@gmail.com>
2019-09-14 23:28:39 +0200

    Home page

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,16 +1,8 @@
 import React, { Component } from "react"
 import Layout from "../components/Layout"
-import Button from "../exemple/Button"
 
 export default class tours extends Component {
   render() {
-    return (
-      <Layout>
-        Hello from tours page
-        <div>
-          <Button>Hello from styled</Button>
-        </div>
-      </Layout>
-    )
+    return <Layout>Hello from tours page</Layout>
   }
 }
